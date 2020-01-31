373a1ede31337ac246ef230c192e15e0cdc2bdb0
baptiste <jahmal.baptiste@ensta-paristech.fr>
2020-01-31 20:02:10 +0100

    Hough again...

--- Tracking_MeanShift.py
+++ Tracking_MeanShift.py
@@ -5,28 +5,25 @@ from utils import *
 
 
 # Algorithm improvements
-updating           = 0
-frame_masking      = 1
-hsv_input          = 0
-gradient_computing = 1
+hsv_input        = 0
+updating         = 0
+frame_masking    = 1
+hough_tracker    = 1
+argmax_computing = 1
+
 GRADIENT_CHANNEL   = 2
 GRADIENT_THRESHOLD = 50.
+NUM_MAX_POINTS     = 2
 
-first_frame_show = 0
-video_show       = 1
 
 roi_defined = 0
 
 def define_ROI(event, x, y, flags, param):
     global r, c, w, h, roi_defined
-    # if the left mouse button was clicked, 
-    # record the starting ROI coordinates 
     if event == cv2.EVENT_LBUTTONDOWN:
         r = x
         c = y
         roi_defined = 0
-    # if the left mouse button was released,
-    # record the ROI coordinates and dimensions
     elif event == cv2.EVENT_LBUTTONUP:
         r2, c2 = x, y
         w = abs(r2 - r)
@@ -83,29 +80,11 @@ v_min = 5.
 v_max = 100.
 
 if hsv_input:
-    # Interest measures
-    # Can be useful to calibrate the min_hsv and max_hsv arrays
-    cv2.imshow("First Frame S", first_hsv[:, :, 1])
-    cv2.imshow("First Frame V", first_hsv[:, :, 2])
-
-    first_frame_hist_S = cv2.calcHist([first_hsv], [1], None, [180], [0, 180])
-    first_frame_hist_V = cv2.calcHist([first_hsv], [2], None, [180], [0, 180])
-    plt.subplot(1, 2, 1)
-    plt.title("First Frame S histogram")
-    plt.plot(first_frame_hist_S)
-    plt.subplot(1, 2, 2)
-    plt.title("First Frame V histogram")
-    plt.plot(first_frame_hist_V)
-    plt.show()
-
-    s_min = float(input("Type the minimum value of SATURATION:\n"))
-    s_max = float(input("Type the maximum value of SATURATION:\n"))
-    v_min = float(input("Type the minimum value of VALUE:\n"))
-    v_max = float(input("Type the maximum value of VALUE:\n"))
+    # show the hsv components of the first frame to calibrate the bounds of the mask function
+    s_min, s_max, v_min, v_max = hsv_bound_input(first_hsv)
 
 
 # computation mask of the histogram:
-# Pixels with S<30 or V<20 or V>235 are ignored 
 min_hsv  = np.array([0., s_min, v_min])
 max_hsv  = np.array([180., s_max, v_max])
 
@@ -123,24 +102,7 @@ cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
 # create RTable
 cv2.normalize(roi_mask, roi_mask, 0, 1, cv2.NORM_MINMAX)
 roi_hsv[:, :, GRADIENT_CHANNEL] *= roi_mask
-
 r_table = construct_RTable(roi_hsv, GRADIENT_THRESHOLD)
-
-
-# draw the initial backproject
-while first_frame_show:
-    first_frame_mask    = cv2.inRange(first_hsv, min_hsv, max_hsv)
-    cv2.normalize(first_frame_mask, first_frame_mask, 0, 1, cv2.NORM_MINMAX)
-
-    first_hsv[:, :, 0] *= first_frame_mask
-    cv2.imshow('First frame H Masked', first_hsv[:, :, 0])
-    
-    first_dst = cv2.calcBackProject([first_hsv], [0], roi_hist, [0,180], 1)
-    cv2.imshow('Initial Backproject', first_dst)
-    
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
 
 
 # Setup the termination criteria: either 10 iterations,
@@ -149,84 +111,85 @@ term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 20, 1 )
 
 #---------------------------------------------------------------------------------------------
 
-if video_show:
-    cpt = 1
-    while(1):
-        ret, current_frame  = cap.read()
+cpt = 1
+while(1):
+    ret, current_frame  = cap.read()
 
-        if ret == True:		
-            current_frame_clone = current_frame.copy()
+    if ret == True:		
+        current_frame_clone = current_frame.copy()
 
-            frame_hsv = cv2.cvtColor(current_frame, cv2.COLOR_BGR2HSV)
+        frame_hsv = cv2.cvtColor(current_frame, cv2.COLOR_BGR2HSV)
 
-            if gradient_computing:
-                Dx = cv2.Sobel(frame_hsv[:, :, GRADIENT_CHANNEL], cv2.CV_64F, 1, 0)
-                Dy = cv2.Sobel(frame_hsv[:, :, GRADIENT_CHANNEL], cv2.CV_64F, 0, 1)
-                G  = np.sqrt(Dx*Dx + Dy*Dy)
+        Dx, Dy      = image_gradient(frame_hsv, channel=GRADIENT_CHANNEL)
+        grad_module = gradient_module(Dx, Dy)
 
-                cv2.normalize(G, G, 0, 255, cv2.NORM_MINMAX)
-                G_mask = cv2.inRange(G, np.array([GRADIENT_THRESHOLD]), np.array([255.]))
+        #cv2.normalize(G, G, 0, 255, cv2.NORM_MINMAX)
+        grad_mask = cv2.inRange(grad_module, np.array([GRADIENT_THRESHOLD]), np.array([np.inf]))
 
-                O     = np.arctan2(Dy, Dx)
-                O_cos = 0.5*(np.cos(O) + 1)*G_mask #normalized to [0, 255]
-                O_sin = 0.5*(np.sin(O) + 1)*G_mask #normalized to [0, 255]
+        orient = np.arctan2(Dy, Dx)
+        orient_cos = 0.5*(np.cos(orient) + 1)*grad_mask #normalized to [0, 255]
+        orient_sin = 0.5*(np.sin(orient) + 1)*grad_mask #normalized to [0, 255]
 
-                Orientation = np.zeros((O.shape[0], O.shape[1], 3))
-                Orientation[:, :, 0] = O_cos
-                Orientation[:, :, 1] = O_sin
-                Orientation[:, :, 2] = 255 - G_mask
+        orient_img = np.zeros((orient.shape[0], orient.shape[1], 3))
+        orient_img[:, :, 0] = orient_cos
+        orient_img[:, :, 1] = orient_sin
+        orient_img[:, :, 2] = 255 - grad_mask
 
-                cv2.imshow('Orientations', Orientation)
+        cv2.imshow('Orientations', orient_img)
 
-                hough = compute_hough(frame_hsv, O, G_mask, r_table).astype(float)
-                cv2.normalize(hough, hough, 0, 255, cv2.NORM_MINMAX)
-                cv2.imshow('Hough Transform', hough)
-                
+        hough = compute_hough(frame_hsv, orient, grad_mask, r_table).astype(float)
+        cv2.normalize(hough, hough, 0, 255, cv2.NORM_MINMAX)
+        cv2.imshow('Hough Transform', hough)
 
-            if frame_masking:
-                frame_mask    = cv2.inRange(frame_hsv, min_hsv, max_hsv)
-                cv2.normalize(frame_mask, frame_mask, 0, 1, cv2.NORM_MINMAX)
-                frame_hsv[:, :, 0] *= frame_mask
-                #cv2.imshow('Frame Mask', frame_mask)
-                #cv2.imshow('Frame H', hsv[:, :, 0])
+        if hough_tracker:
+            if argmax_computing:
+                track_window = hough_window(hough, num_points=NUM_MAX_POINTS)
+            else:
+                ret, track_window = cv2.meanShift(hough, track_window, term_crit)
+        
 
-            
-            # Backproject the model histogram roi_hist onto the
-            # current image hsv, i.e. dst(x, y) = roi_hist(hsv(0, x, y))
-            dst = cv2.calcBackProject([frame_hsv], [0], roi_hist, [0,180], 1)
+        if frame_masking:
+            frame_mask = cv2.inRange(frame_hsv, min_hsv, max_hsv)
+            cv2.normalize(frame_mask, frame_mask, 0, 1, cv2.NORM_MINMAX)
+            frame_hsv[:, :, 0] *= frame_mask
+        
+        # Backproject the model histogram roi_hist onto the
+        # current image hsv, i.e. dst(x, y) = roi_hist(hsv(0, x, y))
+        dst = cv2.calcBackProject([frame_hsv], [0], roi_hist, [0,180], 1)
 
-            # Draw the backproject of the current image
-            #cv2.imshow('Backproject', dst)
+        # Draw the backproject of the current image
+        cv2.imshow('Backproject', dst)
 
+
+        if not hough_tracker:
             # apply meanshift to dst to get the new location
             ret, track_window = cv2.meanShift(dst, track_window, term_crit)
 
-            # Draw a blue rectangle on the current image
-            x, y, w, h    = track_window
-            frame_tracked = cv2.rectangle(current_frame, (x, y), (x+w, y+h), (255, 0, 0) ,2)
-            cv2.imshow('Sequence', frame_tracked)
+        # Draw a blue rectangle on the current image
+        x, y, w, h    = track_window
+        frame_tracked = cv2.rectangle(current_frame, (x, y), (x+w, y+h), (0, 0, 255) ,2)
+        cv2.imshow('Sequence', frame_tracked)
 
-            if updating:
-                roi      = current_frame_clone[y:y+h, x:x+w]
-                roi_hsv  = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-                roi_mask = cv2.inRange(roi_hsv, min_hsv, max_hsv)
-                roi_hist = cv2.calcHist([roi_hsv], [0], roi_mask, [180], [0,180])
-                cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
-                cv2.normalize(roi_mask, roi_mask, 0, 1, cv2.NORM_MINMAX)
-                
-                roi_hsv[:, :, 0] *= roi_mask
-                cv2.imshow("ROI", roi)
-                cv2.imshow('ROI HSV', roi_hsv[:, :, 0])
+        if updating:
+            # Does not help...
+            roi      = current_frame_clone[y:y+h, x:x+w]
+            roi_hsv  = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+            roi_mask = cv2.inRange(roi_hsv, min_hsv, max_hsv)
+            roi_hist = cv2.calcHist([roi_hsv], [0], roi_mask, [180], [0,180])
+            cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+            cv2.normalize(roi_mask, roi_mask, 0, 1, cv2.NORM_MINMAX)
+            
+            roi_hsv[:, :, 0] *= roi_mask
 
 
-            k = cv2.waitKey(60) & 0xff
-            if k == 27:
-                break
-            elif k == ord('s'):
-                cv2.imwrite('Frame_%04d.png'%cpt, frame_tracked)
-            cpt += 1
-        else:
+        k = cv2.waitKey(60) & 0xff
+        if k == 27:
             break
+        elif k == ord('s'):
+            cv2.imwrite('Frame_%04d.png'%cpt, frame_tracked)
+        cpt += 1
+    else:
+        break
 
 cv2.destroyAllWindows()
 cap.release()
--- utils.py
+++ utils.py
@@ -4,32 +4,47 @@ import matplotlib.pyplot as plt
 from collections import OrderedDict
 
 
-def define_ROI(event, x, y, flags, param):
-    global r, c, w, h, ROI_DEFINED
-    # if the left mouse button was clicked,
-    # record the starting ROI coordinates
-    if event == cv2.EVENT_LBUTTONDOWN:
-        r = x
-        c = y
-        ROI_DEFINED = False
-    # if the left mouse button was released,
-    # record the ROI coordinates and dimensions
-    elif event == cv2.EVENT_LBUTTONUP:
-        r2, c2 = x, y
-        w = abs(r2 - r)
-        h = abs(c2 - c)
-        r = min(r, r2)
-        c = min(c, c2)
-        ROI_DEFINED = True
+
+def hsv_bound_input(hsv):
+    cv2.imshow("Frame S", hsv[:, :, 1])
+    cv2.imshow("Frame V", hsv[:, :, 2])
+
+    frame_hist_S = cv2.calcHist([hsv], [1], None, [180], [0, 180])
+    frame_hist_V = cv2.calcHist([hsv], [2], None, [180], [0, 180])
+    plt.subplot(1, 2, 1)
+    plt.title("Frame S histogram")
+    plt.plot(frame_hist_S)
+    plt.subplot(1, 2, 2)
+    plt.title("Frame V histogram")
+    plt.plot(frame_hist_V)
+    plt.show()
+
+    s_min = float(input("Type the minimum value of SATURATION:\n"))
+    s_max = float(input("Type the maximum value of SATURATION:\n"))
+    v_min = float(input("Type the minimum value of VALUE:\n"))
+    v_max = float(input("Type the maximum value of VALUE:\n"))
+
+    return s_min, s_max, v_min, v_max
 
 
-def image_gradient(img, k=0):
+def show_frame(hsv, min_hsv, max_hsv, roi_hist):
+    frame_mask    = cv2.inRange(hsv, min_hsv, max_hsv)
+    cv2.normalize(frame_mask, frame_mask, 0, 1, cv2.NORM_MINMAX)
+
+    hsv[:, :, 0] *= frame_mask
+    cv2.imshow('Frame H Masked', hsv[:, :, 0])
+    
+    dst = cv2.calcBackProject([hsv], [0], roi_hist, [0,180], 1)
+    cv2.imshow('Backproject', dst)
+
+
+def image_gradient(img, channel=0):
     """
     calculate the gradient of an hsv_image in a kernel k.
     :return:
     """
-    Dx = cv2.Sobel(img[:, :, k], cv2.CV_64F, 1, 0)
-    Dy = cv2.Sobel(img[:, :, k], cv2.CV_64F, 0, 1)
+    Dx = cv2.Sobel(img[:, :, channel], cv2.CV_64F, 1, 0)
+    Dy = cv2.Sobel(img[:, :, channel], cv2.CV_64F, 0, 1)
     return Dx, Dy
 
 
@@ -51,7 +66,7 @@ def gradient_orientation(Dx, Dy):
     :return:
     """
     orientation = np.arctan2(Dy, Dx)
-    orientation *= (180/np.pi)
+    orientation*= (180/np.pi)
     orientation = np.where(orientation < 0, orientation + 360, orientation)
     return orientation
 
@@ -65,15 +80,6 @@ def quantise_grad_orientation(orientation, angle_step=2):
     """
     return np.floor(orientation/angle_step)
 
-
-#def edges_image(img, k=2):
-#    """
-#    Find the edge of an image.
-#    :param img:
-#    :param k:
-#    :return:
-#    """
-#    return cv2.Canny(img[:, :, k], 100, 200)
 
 
 def image_ref_point(edges_img):
@@ -146,7 +152,7 @@ def construct_RTable(obj, gradient_threshold, channel=2):
     return r_table
 
 
-def compute_hough(image, image_grad_orient, image_grad_mask, r_table, channel=2):
+def compute_hough(image, image_grad_orient, image_grad_mask, r_table):
     # compute the orientation of the gradient in degrees
     grad_orient = image_grad_orient*(180./np.pi)
     grad_orient = np.where(grad_orient < 0, grad_orient + 360, grad_orient)
@@ -155,35 +161,50 @@ def compute_hough(image, image_grad_orient, image_grad_mask, r_table, channel=2)
     quantised_grad_orient = quantise_grad_orientation(grad_orient, 2)
 
     # compute the appearances of all the edge points of the image
-    edges_pos              = np.nonzero(image_grad_mask)
-    appearances            = -1*np.ones_like(image[:, :, channel]) # appearance of -1 for the points that are not edges
-    appearances[edges_pos] = quantised_grad_orient[edges_pos]
+    edges_pos = np.transpose(np.nonzero(image_grad_mask))
+
+    hough = np.zeros_like(image_grad_orient)
+    for i, pos_i in enumerate(edges_pos):
+        appearance_index = quantised_grad_orient[tuple(pos_i)]
+        if appearance_index in list(r_table.keys()):
+            for j, pos_j in enumerate(r_table[appearance_index]):
+                pos = np.floor(pos_i + pos_j).astype(int)
+                if pos[0]<0 or pos[0]>=image.shape[0] or pos[1]<0 or pos[1]>=image.shape[1]:
+                    continue
+                hough[tuple(pos)] += 1
     
-    # create an array of the positions of the image points (initialised at (0, 0) for all points)
-    positions    = np.empty(image[:, :, channel].shape[0]*image[:, :, channel].shape[1], dtype=tuple)
-    positions[:] = [(0, 0)]
-    positions    = np.reshape(positions, image[:, :, channel].shape)
-
-    # filling of the positions of the edge points alone    
-    tuple_edges_pos      = [(edges_pos[0][k], edges_pos[1][k]) for k in range(edges_pos[0].shape[0])]
-    positions[edges_pos] = tuple_edges_pos
-
-    # definition and vectorization of the vote function
-    vote_func = np.vectorize(lambda pos : len(r_table[appearances[pos]]))
-
-    # hough computation
-    #hough = np.zeros_like(image[:, :, channel])
-    hough = vote_func(positions) #Takes a lot of time...
 
     ### ALTERNATE HOUGH COMPUTATION ###
-    ### (MUCH MORE TIME-CONSUMING)  ###
-    #for i, pos_i in enumerate(image_edges_pos):
-    #    appearance_index = image_quantised_grad_orient[tuple(pos_i)]
-    #    if appearance_index in list(r_table.keys()):
-    #        for j, pos_j in enumerate(r_table[appearance_index]):
-    #            pos = np.floor(pos_i + pos_j).astype(int)
-    #            if pos[0]<0 or pos[0]>=image.shape[0] or pos[1]<0 or pos[1]>=image.shape[1]:
-    #                continue
-    #            hough[tuple(pos)] += 1
+    ###       (UNFINISHED...)       ###
+
+    #edges_pos              = np.nonzero(image_grad_mask)
+    #appearances            = -1*np.ones_like(image_grad_orient) # appearance of -1 for the points that are not edges
+    #appearances[edges_pos] = quantised_grad_orient[edges_pos]
+    #
+    ## create an array of the positions of the image points (initialised at (0, 0) for all points)
+    #positions    = np.empty(image.shape[0]*image.shape[1], dtype=tuple)
+    #positions[:] = [(0, 0)]
+    #positions    = np.reshape(positions, image_grad_orient.shape)
+    #
+    ## filling of the positions of the edge points alone
+    #tuple_edges_pos      = [(edges_pos[0][k], edges_pos[1][k]) for k in range(edges_pos[0].shape[0])]
+    #positions[edges_pos] = tuple_edges_pos
+    #
+    ## definition and vectorization of the vote function
+    #def vote_func(pos):
+    #    return len(r_table[appearances[pos]])
+    #vvote_func = np.vectorize(vote_func)
+    #
+    ## hough computation
+    #hough = vvote_func(positions)
     
     return hough
+
+
+def hough_window(hough, num_points):
+    max_prob_indexes = np.transpose(np.unravel_index(np.argsort(hough, axis=None), hough.shape))[0:num_points]
+    min_pos          = np.amin(max_prob_indexes, 0)
+    max_pos          = np.amax(max_prob_indexes, 0)
+    track_window     = min_pos[0], min_pos[1], max_pos[0]-min_pos[0], max_pos[1]-min_pos[1]
+
+    return track_window
